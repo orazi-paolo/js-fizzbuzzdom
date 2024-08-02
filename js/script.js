46d5e2fa801362ec5d04090e66c674e9dc98db29
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-02 14:55:49 +0200

    added ladder

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*  Scaletta
+1. stampa i numeri da 1 a 100 in console
+2. per i multipli di 3 stampa Fizz al posto del numero
+3. per i multipli di 5 stampa Buzz al posto del numero
+4. per i multipli che sono multipli sia di 3 che di 5 stampa FizzBuzz al posto del numero
+5. crea un elemento che faccia da contenitore nel DOM 
+6. riempi il contenitore con degli elementi via js
+7. applica stili differenti agli elementi aggiunti a seconda del valore inserito che sia un fizz un numero o un buzz
+*/
